dc82096e50e532deb44c110cca6a061f517a5aef
Wu Han <wuhanstudio@hust.edu.cn>
2019-01-07 08:36:20 +0800

    Add ADC

--- attiny10/src/main.c
+++ attiny10/src/main.c
@@ -31,16 +31,47 @@ int main(void)
 	// Initialize PWM
 	TCCR0A =  (1 << COM0A1) | (1 << COM0B1) | (1 << COM0B0) | (1 << WGM00);
 	TCCR0B =  (1 << WGM02)  | (1 << CS00);
-	OCR0A  =  205;  // Duty 80 
-	OCR0B  =  205;  // Duty 20
+	//OCR0A  =  205;  // Duty 80 
+	//OCR0B  =  205;  // Duty 20
 	
+	ADMUX = (1 << MUX1);	 
+	// ADC Enable and prescaler of 128
+	// 16000000/128 = 125000
+	ADCSRA = (1<<ADEN)|(1<<ADPS1); //|(1<<ADPS1)|(1<<ADPS0);
+ 
     /* Replace with your application code */
-    while (1) 
+	unsigned char val;
+		
+	while (1) 
     {
-		_delay_ms(1000);
-		PORTB &= ~(1 << PORTB2);
-		_delay_ms(1000);
-		PORTB |= 1 << PORTB2;
+		ADCSRA |= (1<<ADSC);
+		while(ADCSRA & (1<<ADSC));
+		val = (unsigned char)ADCL;
+		OCR0A = val;
+		OCR0B = val;
+		
+		//_delay_ms(1000);
+		//PORTB &= ~(1 << PORTB2);
+
+		//_delay_ms(1000);
+		//PORTB |= 1 << PORTB2;
+	}
+}
+
+void pwm_led(){
+	int i;
+	for (i = 0; i< 256; i ++)
+	{
+		OCR0A  =  i;  // Duty 80
+		OCR0B  =  i;  // Duty 20
+		_delay_ms(10);
+	}
+	_delay_ms(200);
+	for (i = 255; i> 0; i --)
+	{
+		OCR0A  =  i;  // Duty 80
+		OCR0B  =  i;  // Duty 20
+		_delay_ms(10);
 	}
 }
 
